0bdc347a854c5d3918286f7e5d6a6bca88028770
Raj Patel <rajpatel@Rajs-Air.attlocal.net>
2022-11-08 13:37:26 -0600

    created the backend

---
+++ src/main/java/com/example/rrairline/RRAirlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.rrairline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RRAirlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RRAirlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rrairline/RRAirlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rrairline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RRAirlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
