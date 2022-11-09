47ec01b5d640e6fd0d0ee1190034ee93c620485d
Austin313 <32691123+Austin313@users.noreply.github.com>
2022-11-09 17:10:38 -0600

    Update FlightController.java

--- src/main/java/com/example/rrairline/controller/FlightController.java
+++ src/main/java/com/example/rrairline/controller/FlightController.java
@@ -1,12 +1,21 @@
 package com.example.rrairline.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.rrairline.dto.FlightInfo;
 import com.example.rrairline.model.Flight;
 import com.example.rrairline.service.FlightService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 @RestController
 @CrossOrigin(origins = {"http://localhost:4200","https://rrairline.azurewebsites.net"})
 public class FlightController {
@@ -18,14 +27,18 @@ public class FlightController {
         this.flightService = flightService;
     }
 
-    @GetMapping("/api/flights/departure/{departure}")
-    public List<Flight> getAllDeparturesByCity(@PathVariable("departure") String departure){
-        return flightService.getFlightByDeparture(departure);
+    @PostMapping("/api/flights/departure")
+    public ResponseEntity<List<Flight>> getAllDeparturesByCity(@RequestBody FlightInfo flightInfo){
+    	List<Flight> list = new ArrayList<Flight>();
+    	list = flightService.getFlightByDeparture(flightInfo.getDeparture());
+        return new ResponseEntity<List<Flight>>(list, HttpStatus.OK);
     }
 
-    @GetMapping("/api/flights")
-    public List<Flight> getAllDeparturesAndArrivalsByCity(@RequestBody FlightInfo flightInfo){
-        return flightService.getFlightByDepartureAndArrival(flightInfo.getDeparture(), flightInfo.getArrival());
+    @PostMapping("/api/flights")
+    public ResponseEntity<List<Flight>> getAllDeparturesAndArrivalsByCity(@RequestBody FlightInfo flightInfo){
+    	List<Flight> list = new ArrayList<Flight>();
+    	list = flightService.getFlightByDepartureAndArrival(flightInfo.getDeparture(), flightInfo.getArrival());
+        return new ResponseEntity<List<Flight>>(list, HttpStatus.OK);
     }
 
 }
